253b635a1ab83b76d19a81bfd5eadf6c1a785bab
Urjaaaaa <urjaalwadkar782000@gmail.com>
2023-11-29 09:52:41 +0530

    first file added

---
+++ cove.py
@@ -0,0 +1,9 @@
+# Taking input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Adding the two numbers
+sum_result = num1 + num2
+
+# Displaying the result
+print(f"The sum of {num1} and {num2} is {sum_result}")
